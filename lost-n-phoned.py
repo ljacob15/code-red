b35fdb1ab49ac5b44a8b92a53cc274b04dfd8327
keanemind <keanemind@gmail.com>
2017-11-04 17:56:17 -0500

    Linter fixes
    Fix indention, comment out unused variable.

--- lost-n-phoned.py
+++ lost-n-phoned.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def message_received():
-    from_number = request.values.get('From', None)
+    # from_number = request.values.get('From', None)
 	# Check if from_number is already in the database
     # If not, add them and get contacts from them
-    
+
     resp = MessagingResponse()
     resp.message("I received your message!")
 
     return str(resp)
-  
-  if __name__ == "__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
