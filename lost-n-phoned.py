cfa8aa2debe5b3e3cb957ec43edb559c244ee9dd
Rodney <rodney.moreno@utexas.edu>
2017-11-04 21:21:03 -0500

    Add find link function that is returned to user

--- lost-n-phoned.py
+++ lost-n-phoned.py
@@ -1,7 +1,31 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 
 app = Flask(__name__)
+
+def google_link():
+        # Use the client_secret.json file to identify the application requesting
+        # authorization. The client ID (from that file) and access scopes are required.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        'client_secret.json',
+        'https://www.googleapis.com/auth/contacts.readonly')
+
+    # Indicate where the API server will redirect the user after the user completes
+    # the authorization flow. The redirect URI is required.
+    flow.redirect_uri = 'https://www.google.com'
+
+    # Generate URL for request to Google's OAuth 2.0 server.
+    # Use kwargs to set optional request parameters.
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true')
+
+    return authorization_url
 
 @app.route("/", methods=['GET', 'POST'])
 def message_received():
@@ -10,7 +34,8 @@ def message_received():
     # If not, add them and get contacts from them
 
     resp = MessagingResponse()
-    resp.message("I received your message!")
+    message = ("Please click the link below: " + str(google_link()))
+    resp.message(message)
 
     return str(resp)
 
