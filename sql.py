f1fd9311f4d84dc03b0fbb905dfb549331ff01cd
ljacob15 <ljacob15@gmail.com>
2017-11-05 05:24:48 -0600

    test 3

--- sql.py
+++ sql.py
@@ -1,7 +1,10 @@
 """ Process the user in the database """
 
-import mysql.connector
-from mysql.connector import errorcode
+
+import pymysql as sql
+
+# import mysql.connector
+# from mysql.connector import errorcode
 import random as rand
 
 # database = "mainData"
@@ -14,39 +17,46 @@ def process(number):
     """
     assert type(number) == str, "number must be a string"
     connection = connect()
-    cursor = connection.cursor()
+    with connection.cursor() as cursor:
 
-    query = ("SELECT * FROM mainData"
-            "WHERE phoneNumber = %s")
+        # query = ("SELECT * FROM mainData"
+        #         "WHERE phoneNumber = %s")
 
-    cursor.execute(query, (number))
+        query = "SELECT * FROM `mainData` WHERE `phoneNumber` = %s "
 
-    if cursor == []:
-        return(False) #user is not yet in database
-    else:
-        return(True) #user is already in database
+        cursor.execute(query, (number))
+
+        if cursor == []:
+            print("nothing in here")
+            return False #user is not yet in database
+        else:
+            print("something in here")
+            return True #user is already in database
 
 
 def connect():
     """Connect to the database. Returns connection object."""
-    try:
-        return mysql.connector.connect(user='root',
-                                       password='CodeRed#2017',
-                                       database='mydatabase',
-                                       host='129.158.66.189')
 
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
+    return sql.connect(user='root',
+                       password='CodeRed#2017',
+                       database='mydatabase',
+                       host='129.158.66.189',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+    # except mysql.connector.Error as err:
+    #     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    #         print("Something is wrong with your user name or password")
+    #     elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    #         print("Database does not exist")
+    #     else:
+    #         print(err)
 
 
 def validate_passphrase(number, public_key, password_attempt):
-    query = ("SELECT security FROM mainData"
-            "WHERE phoneNumber = %s")
+    # query = ("SELECT security FROM mainData"
+    #         "WHERE phoneNumber = %s")
+
+    query = "SELECT security FROM `mainData` WHERE `phoneNumber` = %s "
 
     cursor.execute(query, (number))
 
@@ -59,8 +69,10 @@ def validate_passphrase(number, public_key, password_attempt):
 
 
 def get_token(number):
-    query = ("SELECT token FROM mainData"
-            "WHERE phoneNumber = %s")
+    # query = ("SELECT token FROM mainData"
+    #         "WHERE phoneNumber = %s")
+
+    query = "SELECT token FROM `mainData` WHERE `phoneNumber` = %s "
 
     cursor.execute(query, (number))
 
@@ -77,10 +89,11 @@ def main(number):
         #key = request_user_private_key()
 
         #create user profile in database
-        add_new_user = ("INSERT INTO mainData VALUES (%s, %s, %s)")
+        add = "INSERT INTO `mainData` VALUES (%s,%s,%s)"
+        # add_new_user = ("INSERT INTO mainData VALUES (%s, %s, %s)")
 
-        cursor.execute(add_new_user, (number, token, key))
-        
+        cursor.execute(add,(number, token, key))
+
     else:
         tries = 5
         while (tries > 0):
