295f0446f3193576e79f74a0b7fe84df387da63e
keanemind <keanemind@gmail.com>
2018-07-22 18:57:57 -0500

    Add security configurations
    Only send cookie over secure channels. Generated URLs now use HTTPS (so the user doesn't have to be redirected from HTTP to HTTPS).

--- lostnphoned/__init__.py
+++ lostnphoned/__init__.py
@@ -7,6 +7,8 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'lostnphoned.sqlite'),
+    SESSION_COOKIE_SECURE=True,
+    PREFERRED_URL_SCHEME='https',
 )
 
 app.config.from_pyfile('config.py', silent=True)
